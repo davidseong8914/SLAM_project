1b83830ea06104bcf3675db91c549d52682e2715
davidseong <davidseong8914@gmail.com>
2024-11-10 11:39:40 -0500

    initialize ouster_runner

---
+++ build/ouster_runner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/david/Desktop/SLAM_project/src/ouster_runner
+# Build directory: /home/david/Desktop/SLAM_project/build/ouster_runner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
